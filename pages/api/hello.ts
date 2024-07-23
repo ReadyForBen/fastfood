d1bc74658002cf6bed920b9bade0eb5e9e16312f
AusAvia2 <144182855+AusAvia2@users.noreply.github.com>
2024-07-23 15:49:45 +1000

    Update hello.ts

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,44 +1,13 @@
-// pages/api/webhook.ts
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
-  message: string
+	name: string
 }
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
+export default function handler(
+	req: NextApiRequest,
+	res: NextApiResponse<Data>
 ) {
-  if (req.method === 'POST') {
-    const { content } = req.body;
-    const discordWebhookUrl = req.headers['discord-webhook-url'];
-
- 
-
-    if (!discordWebhookUrl || typeof discordWebhookUrl !== 'string') {
-      return res.status(400).json({ message: 'Discord webhook URL is required in headers' });
-    }
-
-    try {
-      const response = await fetch(discordWebhookUrl, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ content })
-      });
-
-      if (!response.ok) {
-        return res.status(response.status).json({ message: 'Failed to send message to Discord webhook' });
-      }
-
-      return res.status(200).json({ message: 'Message sent to Discord webhook' });
-    } catch (error) {
-      console.error('Error sending message to Discord webhook:', error);
-      return res.status(500).json({ message: 'Internal server error' });
-    }
-  } else {
-    res.setHeader('Allow', ['POST']);
-    res.status(405).json({ message: `Method ${req.method} not allowed` });
-  }
+	res.status(200).json({ name: 'John Doe' })
 }
